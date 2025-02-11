454289ab3ef95f7f87141b98d3e9df36f4b126f3
Iza <83782683+Dzizzaa@users.noreply.github.com>
2025-02-11 17:32:13 +0100

    schema.sql

---
+++ schema.sql
@@ -0,0 +1,31 @@
+CREATE TABLE Trips (
+    TripID INT PRIMARY KEY IDENTITY(1,1),
+    City VARCHAR(255),
+    Country VARCHAR(255),
+    Start_Date DATE,
+    End_Date DATE,
+    Duration_Days INT,
+    Accommodation_Type VARCHAR(255),
+    Accommodation_Cost FLOAT,
+    Transportation_Type VARCHAR(255),
+    Transportation_Cost FLOAT 
+);
+
+
+
+CREATE TABLE Travelers (
+    TravelerID INT PRIMARY KEY IDENTITY(1,1),
+    Name VARCHAR(255),
+    Age FLOAT,
+    Gender VARCHAR(20),
+    Nationality VARCHAR(255)
+
+);
+
+CREATE TABLE Trip_Travelers (
+    TripID INT,
+    TravelerID INT,
+    PRIMARY KEY (TripID, TravelerID),
+    FOREIGN KEY (TripID) REFERENCES Trips(TripID),
+    FOREIGN KEY (TravelerID) REFERENCES Travelers(TravelerID)
+);
